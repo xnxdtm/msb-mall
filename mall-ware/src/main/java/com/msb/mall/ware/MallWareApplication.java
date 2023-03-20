266a38d3139c6ebcbd7c78abf79af8c07c26a1e9
wj <xnxdtm@163.com>
2023-03-20 22:37:03 +0800

    feat(*): 基础环境搭建完成

--- mall-ware/src/main/java/com/msb/mall/ware/MallWareApplication.java
+++ mall-ware/src/main/java/com/msb/mall/ware/MallWareApplication.java
@@ -1,4 +1,4 @@
-package com.msb.mall.test;
+package com.msb.mall.ware;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class MallProductApplication {
+public class MallWareApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MallProductApplication.class, args);
+        SpringApplication.run(MallWareApplication.class, args);
     }
 
 }
