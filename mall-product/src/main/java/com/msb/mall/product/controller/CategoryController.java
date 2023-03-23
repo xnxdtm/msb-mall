18fe7d7c29f54cfbd039e870b86e8f00628cb479
wj <xnxdtm@163.com>
2023-03-24 06:22:43 +0800

    feat(mall-product): 添加三级分类接口

--- mall-product/src/main/java/com/msb/mall/product/controller/CategoryController.java
+++ mall-product/src/main/java/com/msb/mall/product/controller/CategoryController.java
@@ -1,15 +1,12 @@
 package com.msb.mall.product.controller;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import com.msb.mall.product.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.msb.mall.product.entity.CategoryEntity;
 import com.msb.mall.common.utils.PageUtils;
@@ -84,6 +81,12 @@ public class CategoryController {
 		categoryService.removeByIds(Arrays.asList(catIds));
 
         return R.ok();
+    }
+
+    @GetMapping("/listTree")
+    public R listTree(@RequestParam Map<String, Object> params){
+        List<CategoryEntity> list = categoryService.queryPageWithTree(params);
+        return R.ok().put("data", list);
     }
 
 }
--- mall-product/src/main/java/com/msb/mall/product/service/impl/CategoryServiceImpl.java
+++ mall-product/src/main/java/com/msb/mall/product/service/impl/CategoryServiceImpl.java
@@ -2,7 +2,12 @@ package com.msb.mall.product.service.impl;
 
 import com.msb.mall.product.service.CategoryService;
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -26,4 +31,45 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         return new PageUtils(page);
     }
 
+    /**
+     * 查询所有的类别数据，然后将数据封装为树形结构，便于前端使用
+     *
+     * @param params
+     * @return
+     */
+    @Override
+    public List<CategoryEntity> queryPageWithTree(Map<String, Object> params) {
+        // 1.查询所有的商品分类信息
+        List<CategoryEntity> categoryEntities = baseMapper.selectList(null);
+        // 2.将商品分类信息拆解为树形结构【父子关系】
+        // 第一步遍历出所有的大类  parent_cid = 0
+        List<CategoryEntity> list = categoryEntities.stream().filter(categoryEntity -> categoryEntity.getParentCid() == 0)
+                .map(categoryEntity -> {
+                    // 根据大类找到多有的小类  递归的方式实现
+                    categoryEntity.setChildren(getCategoryChildrens(categoryEntity, categoryEntities));
+                    return categoryEntity;
+                }).sorted(Comparator.comparingInt(entity -> (entity.getSort() == null ? 0 : entity.getSort()))).collect(Collectors.toList());
+        // 第二步根据大类找到对应的所有的小类
+        return list;
+    }
+
+    /**
+     * 查找该大类下的所有的小类  递归查找
+     *
+     * @param categoryEntity   某个大类
+     * @param categoryEntities 所有的类别数据
+     * @return
+     */
+    private List<CategoryEntity> getCategoryChildrens(CategoryEntity categoryEntity
+            , List<CategoryEntity> categoryEntities) {
+        List<CategoryEntity> collect = categoryEntities.stream().filter(entity -> {
+            // 根据大类找到他的直属的小类
+            return entity.getParentCid() == categoryEntity.getCatId();
+        }).map(entity -> {
+            // 根据这个小类递归找到对应的小小类
+            entity.setChildren(getCategoryChildrens(entity, categoryEntities));
+            return entity;
+        }).sorted(Comparator.comparingInt(entity -> (entity.getSort() == null ? 0 : entity.getSort()))).collect(Collectors.toList());
+        return collect;
+    }
 }
